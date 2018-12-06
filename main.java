c844c4269eb5999db5c7eb9af9c5dcff3c60140a
rmcotter <rmcotter@wisc.edu>
2018-12-05 19:10:31 -0600

    Add files via upload

---
+++ main.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class main {
+	public static void main(String[] args) throws IOException
+    {
+		String fileName = "foodItems.txt";
+	// System.out.println("Enter the json filename:");
+    // Scanner s1 = new Scanner(System.in);
+	//String jsonFileName = s1.nextLine();
+     parseFile(fileName);
+     //s1.close();
+    }
+	public static void parseFile(String filePath) throws IOException{
+		//List<Long> targetLongList = sourceLongList.stream().collect(Collectors.toList());
+		List<String> food = (List) Files.lines(Paths.get(filePath))
+				// .map(String::trim)
+				// .map(String::toLowerCase)
+				// .filter(line -> line.contains("filter"))
+				// .count();
+				.collect(Collectors.toList());
+		for (String var : food) 
+		{ 
+			 System.out.println(var);
+		}
+		}
+				
+}
